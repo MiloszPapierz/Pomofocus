507cbaf17b00d8fda2cf43a50c4c4c4b9a2f8704
MiloszPapierz <Milosz.papierz1607@gmail.com>
2023-08-04 21:49:00 +0200

    Add prisma connection

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User as UserEntity } from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<UserEntity[]> {
     return this.appService.getHello();
   }
 }
