34caa30967f5d783c492898a4364e032e36eeff8
MiloszPapierz <Milosz.papierz1607@gmail.com>
2023-08-25 12:10:33 +0200

    Remove test endpoint in app module

--- server/src/modules/app/app.controller.ts
+++ server/src/modules/app/app.controller.ts
@@ -1,15 +1,9 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { User as UserEntity } from '@prisma/client';
-import { JwtGuard } from 'src/modules/auth/guards/jwt.guard';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  @UseGuards(JwtGuard)
-  async getHello(): Promise<UserEntity[]> {
-    return this.appService.getHello();
+  async getHello() {
+    return '';
   }
 }
